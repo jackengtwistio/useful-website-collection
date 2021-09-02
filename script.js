11962d1ce39079745d0d43b9d7f8a6f17ed38bec
jackengtwistio <jtwist108@gmail.com>
2021-09-02 21:01:22 +0800

    navbar updated

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-// const navInstruction = document.getElementById('navInstruction');
-
-// function showElement(navInstruction){
-//   navInstruction.textContent.push('Click them for more resources');
-// }
-// setInterval(showElement, 1000);
-
-// // ---------------------------------------------------------------
-
-// const emailInput = document.getElementById('exampleFormControlInput1')
-// const textArea = document.getElementById('exampleFormControlTextarea1')
-
-// emailInput.addEventListener('click', event =>{
-//   event.target.className = 'form-control highlight';
-// });
-// textArea.addEventListener('click', event =>{
-//   event.target.className = 'form-control highlight';
-// });
-// ----------------------------------------------------------
-// $('nav').hide()
-$('#sug-submit').click(function(){
-  const sug=$('#sug-input').val();
-  $('#sug').text(sug);
-  // $('nav').fadeIn().delay(3000).slideUp();
-});
-$('.dropdown-toggle').addEventListener('click',
-  ()=>{
-    $('.dropdown-menu').style.display = 'blok';
-    // if($('.dropdown-menu').style.display == 'none'){
-    //   $('.dropdown-menu').style.display = 'blok'
-    // }else{
-      // $('.dropdown-toggle').textContent = 'hide list';
-      // $('.dropdown-menu').style.display = 'none'
-    // }
+var navbar = new Vue({
+  el: '.navbars',
+  data: {
+    ITWebLinks: [
+      '<a href="https://bento.io/topic/web/tutorials">Bento</a>',
+      '<a href="https://learn.shayhowe.com/advanced-html-css/performance-organization/">Shayhowe</a>',
+      '<a href="https://teachyourselfcs.com/#algorithms">Teachyourselfcs</a>',
+      '<a href="https://app.pluralsight.com/library/courses/css-creating-page-layouts/table-of-contents">Pluralsight</a>',
+      '<a href="https://fullstackopen.com/en/about">Fullstackopen</a>',
+      '<a href="https://scrimba.com/learn/frontend#">Scrimba</a>',
+      '<a href="https://frontendmasters.com/courses/">Frontendmasters</a>',
+      '<a href="https://cs50.harvard.edu/web/2020/weeks/0/">Harvardweb</a>',
+      '<a href="https://dash.generalassemb.ly/projects">GA</a>',
+      '<a href="https://www.sitepoint.com/">Sitepoint</a>',
+    ],
+    generalWebLinks: [
+      '<a href="https://online-learning.harvard.edu/">Harvard online course</a>',
+      '<a href="https://www.openculture.com/">Openculture(it collects other webs)</a>',
+      '<a href="https://www.ted.com/">TED</a>',
+      '<a href="https://www.edx.org/">EDX</a>',
+      '<a href="https://www.khanacademy.org/">khanacademy</a>',
+      '<a href="https://www.udemy.com/">Udemy</a>',
+      '<a href="https://www.greatlearning.edu.hk/">Great learning</a>'
+    ]
   }
-)
+});
